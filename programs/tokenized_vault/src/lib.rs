8eed689f45aa7b668e3941dfeff2f5be098a7792
tarunsharma <tarun.kr.sharma@hotmail.com>
2024-09-27 11:09:55 +0400

    fix: reverted the program ids

--- programs/tokenized_vault/src/lib.rs
+++ programs/tokenized_vault/src/lib.rs
@@ -12,7 +12,7 @@ pub use state::*;
 pub use instructions::*;
 // pub use utils::*;
 //
-declare_id!("CNyqz3mqw6koNmAe7rn2xHGHAS9ftXUNQohwHSiXhJLQ");
+declare_id!("8eDcyX8Z8yZXBQsuatwxDC1qzGbuUbP7wGERDBQoPmBH");
 
 #[program]
 pub mod tokenized_vault {
