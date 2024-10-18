54a8f1d2f97530a950ee455cef56a1872b544f63
Davit Mkhitaryan <davitmkhitaryanit@gmail.com>
2024-10-18 18:28:07 +0400

    Test automation: Added remaining tests to standard vault deposit test suite

--- tests/integration/tests/standart-vault/deposit.test.ts
+++ tests/integration/tests/standart-vault/deposit.test.ts
@@ -15,24 +15,33 @@ import {
   connection,
 } from "../../setups/globalSetup";
 
-describe.only("Standard Vault: Deposit Tests", () => {
+describe("Standard Vault: Deposit Tests", () => {
   let vaultsAndReportingAdmin: anchor.web3.Keypair;
   let userOne: anchor.web3.Keypair;
   let userOneTokenAccount: anchor.web3.PublicKey;
   let userOneVaultOneSharesAccount: anchor.web3.PublicKey;
   let userOneVaultTwoSharesAccount: anchor.web3.PublicKey;
+  let userOneVaultThreeSharesAccount: anchor.web3.PublicKey;
+  let userTwo: anchor.web3.Keypair;
+  let userTwoTokenAccount: anchor.web3.PublicKey;
+  let userTwoVaultOneSharesAccount: anchor.web3.PublicKey;
+  let userTwoVaultTwoSharesAccount: anchor.web3.PublicKey;
   let underlyingMint: anchor.web3.PublicKey;
   let underlyingMintOwner: anchor.web3.Keypair;
 
   // First - For Role Admin
   let vaultOne: anchor.web3.PublicKey;
   let vaultTwo: anchor.web3.PublicKey;
+  let vaultThree: anchor.web3.PublicKey;
   let sharesMintOne: anchor.web3.PublicKey;
   let sharesMintTwo: anchor.web3.PublicKey;
+  let sharesMintThree: anchor.web3.PublicKey;
   let metadataAccountOne: anchor.web3.PublicKey;
   let metadataAccountTwo: anchor.web3.PublicKey;
+  let metadataAccountThree: anchor.web3.PublicKey;
   let vaultTokenAccountOne: anchor.web3.PublicKey;
   let vaultTokenAccountTwo: anchor.web3.PublicKey;
+  let vaultTokenAccountThree: anchor.web3.PublicKey;
   let strategyOne: anchor.web3.PublicKey;
   let strategyTokenAccountOne: anchor.web3.PublicKey;
   let strategyTwo: anchor.web3.PublicKey;
@@ -43,6 +52,9 @@ describe.only("Standard Vault: Deposit Tests", () => {
   let userOneUnderlyingTokenCurrentBalance: number = 0;
   let userOneVaultOneSharesCurrentBalance: number = 0;
   let userOneVaultTwoSharesCurrentBalance: number = 0;
+  let userTwoUnderlyingTokenCurrentBalance: number = 0;
+  let userTwoVaultOneSharesCurrentBalance: number = 0;
+  let userTwoVaultTwoSharesCurrentBalance: number = 0;
   let vaultOneCurrentTokenBalance: number = 0;
   let vaultTwoCurrentTokenBalance: number = 0;
 
@@ -56,6 +68,7 @@ describe.only("Standard Vault: Deposit Tests", () => {
     console.log("-------Before Step Started-------");
     vaultsAndReportingAdmin = anchor.web3.Keypair.generate();
     userOne = anchor.web3.Keypair.generate();
+    userTwo = anchor.web3.Keypair.generate();
     underlyingMintOwner = rolesAdmin;
 
     console.log("Vault Program ID:", vaultProgram.programId.toBase58());
@@ -74,6 +87,7 @@ describe.only("Standard Vault: Deposit Tests", () => {
     const publicKeysList = [
       vaultsAndReportingAdmin.publicKey,
       userOne.publicKey,
+      userTwo.publicKey,
     ];
     for (const publicKey of publicKeysList) {
       await airdrop({
@@ -149,6 +163,28 @@ describe.only("Standard Vault: Deposit Tests", () => {
         config: vaultConfig,
       });
 
+    [
+      vaultThree,
+      sharesMintThree,
+      metadataAccountThree,
+      vaultTokenAccountThree,
+    ] = await initializeVault({
+      vaultProgram,
+      underlyingMint,
+      vaultIndex: 13,
+      signer: vaultsAndReportingAdmin,
+      config: vaultConfig,
+    });
+
+    await vaultProgram.methods
+      .shutdownVault()
+      .accounts({
+        vault: vaultThree,
+        signer: vaultsAndReportingAdmin.publicKey,
+      })
+      .signers([vaultsAndReportingAdmin])
+      .rpc();
+
     console.log("All Vaults initialized successfully");
 
     [strategyOne, strategyTokenAccountOne] = await initializeSimpleStrategy({
@@ -214,15 +250,21 @@ describe.only("Standard Vault: Deposit Tests", () => {
 
     console.log("All Strategies added to corresponding vaults successfully");
 
-    // Create whitelisted user token and shares accounts and mint underlying tokens
     userOneTokenAccount = await token.createAccount(
       connection,
       userOne,
       underlyingMint,
       userOne.publicKey
     );
+    userTwoTokenAccount = await token.createAccount(
+      connection,
+      userTwo,
+      underlyingMint,
+      userTwo.publicKey
+    );
     console.log("User token accounts created successfully");
-    const mintAmount = 1000000000000;
+
+    const mintAmount = 10000000000000;
     await token.mintTo(
       connection,
       underlyingMintOwner,
@@ -231,7 +273,17 @@ describe.only("Standard Vault: Deposit Tests", () => {
       underlyingMintOwner.publicKey,
       mintAmount
     );
+    await token.mintTo(
+      connection,
+      underlyingMintOwner,
+      underlyingMint,
+      userTwoTokenAccount,
+      underlyingMintOwner.publicKey,
+      mintAmount
+    );
     userOneUnderlyingTokenCurrentBalance = mintAmount;
+    userTwoUnderlyingTokenCurrentBalance = mintAmount;
+
     console.log("Minted 1000000000000 underlying tokens to users successfully");
 
     userOneVaultOneSharesAccount = await token.createAccount(
@@ -246,8 +298,26 @@ describe.only("Standard Vault: Deposit Tests", () => {
       sharesMintTwo,
       userOne.publicKey
     );
+    userOneVaultThreeSharesAccount = await token.createAccount(
+      connection,
+      userOne,
+      sharesMintThree,
+      userOne.publicKey
+    );
+    userTwoVaultOneSharesAccount = await token.createAccount(
+      connection,
+      userTwo,
+      sharesMintOne,
+      userTwo.publicKey
+    );
+    userTwoVaultTwoSharesAccount = await token.createAccount(
+      connection,
+      userTwo,
+      sharesMintTwo,
+      userTwo.publicKey
+    );
     console.log(
-      "Created all required shares accounts to all users successfully"
+      "Created all required shares accounts for all users successfully"
     );
     console.log("-------Before Step Finished-------");
   });
@@ -267,11 +337,9 @@ describe.only("Standard Vault: Deposit Tests", () => {
       .signers([userOne])
       .rpc();
 
-    userOneUnderlyingTokenCurrentBalance =
-      userOneUnderlyingTokenCurrentBalance - depositAmount;
-    userOneVaultOneSharesCurrentBalance =
-      userOneVaultOneSharesCurrentBalance + depositAmount;
-    vaultOneCurrentTokenBalance = vaultOneCurrentTokenBalance + depositAmount;
+    userOneUnderlyingTokenCurrentBalance -= depositAmount;
+    userOneVaultOneSharesCurrentBalance += depositAmount;
+    vaultOneCurrentTokenBalance += depositAmount;
 
     const vaultTokenAccountInfo = await token.getAccount(
       connection,
@@ -329,12 +397,9 @@ describe.only("Standard Vault: Deposit Tests", () => {
       .signers([userOne])
       .rpc();
 
-    userOneUnderlyingTokenCurrentBalance =
-      userOneUnderlyingTokenCurrentBalance - totalDepositAmount;
-    userOneVaultOneSharesCurrentBalance =
-      userOneVaultOneSharesCurrentBalance + totalDepositAmount;
-    vaultOneCurrentTokenBalance =
-      vaultOneCurrentTokenBalance + totalDepositAmount;
+    userOneUnderlyingTokenCurrentBalance -= totalDepositAmount;
+    userOneVaultOneSharesCurrentBalance += totalDepositAmount;
+    vaultOneCurrentTokenBalance += totalDepositAmount;
 
     const vaultTokenAccountInfo = await token.getAccount(
       connection,
@@ -379,13 +444,9 @@ describe.only("Standard Vault: Deposit Tests", () => {
       .signers([userOne])
       .rpc();
 
-    console.log("Deposit success");
-
-    userOneUnderlyingTokenCurrentBalance =
-      userOneUnderlyingTokenCurrentBalance - depositAmount;
-    userOneVaultTwoSharesCurrentBalance =
-      userOneVaultTwoSharesCurrentBalance + depositAmount;
-    vaultTwoCurrentTokenBalance = vaultTwoCurrentTokenBalance + depositAmount;
+    userOneUnderlyingTokenCurrentBalance -= depositAmount;
+    userOneVaultTwoSharesCurrentBalance += depositAmount;
+    vaultTwoCurrentTokenBalance += depositAmount;
 
     const vaultTokenAccountInfo = await token.getAccount(
       connection,
@@ -443,12 +504,9 @@ describe.only("Standard Vault: Deposit Tests", () => {
       .signers([userOne])
       .rpc();
 
-    userOneUnderlyingTokenCurrentBalance =
-      userOneUnderlyingTokenCurrentBalance - totalDepositAmount;
-    userOneVaultTwoSharesCurrentBalance =
-      userOneVaultTwoSharesCurrentBalance + totalDepositAmount;
-    vaultTwoCurrentTokenBalance =
-      vaultTwoCurrentTokenBalance + totalDepositAmount;
+    userOneUnderlyingTokenCurrentBalance -= totalDepositAmount;
+    userOneVaultTwoSharesCurrentBalance += totalDepositAmount;
+    vaultTwoCurrentTokenBalance += totalDepositAmount;
 
     const vaultTokenAccountInfo = await token.getAccount(
       connection,
@@ -466,6 +524,306 @@ describe.only("Standard Vault: Deposit Tests", () => {
     assert.strictEqual(
       userSharesAccountInfo.amount.toString(),
       userOneVaultTwoSharesCurrentBalance.toString()
+    );
+
+    const userTokenAccountInfo = await token.getAccount(
+      connection,
+      userOneTokenAccount
+    );
+    assert.strictEqual(
+      userTokenAccountInfo.amount.toString(),
+      userOneUnderlyingTokenCurrentBalance.toString()
+    );
+  });
+
+  it("Depositing less then minimum deposit amount should revert", async function () {
+    this.qaseId(63);
+    const depositAmount = 999999999;
+
+    try {
+      await vaultProgram.methods
+        .deposit(new BN(depositAmount))
+        .accounts({
+          vault: vaultOne,
+          user: userOne.publicKey,
+          userTokenAccount: userOneTokenAccount,
+          userSharesAccount: userOneVaultOneSharesAccount,
+        })
+        .signers([userOne])
+        .rpc();
+    } catch (err) {
+      expect(err.message).contains(
+        "MinDepositNotReached. Error Number: 6016. Error Message: Min deposit not reached."
+      );
+    }
+
+    const vaultTokenAccountInfo = await token.getAccount(
+      connection,
+      vaultTokenAccountOne
+    );
+    assert.strictEqual(
+      vaultTokenAccountInfo.amount.toString(),
+      vaultOneCurrentTokenBalance.toString()
+    );
+
+    const userSharesAccountInfo = await token.getAccount(
+      connection,
+      userOneVaultOneSharesAccount
+    );
+    assert.strictEqual(
+      userSharesAccountInfo.amount.toString(),
+      userOneVaultOneSharesCurrentBalance.toString()
+    );
+
+    const userTokenAccountInfo = await token.getAccount(
+      connection,
+      userOneTokenAccount
+    );
+    assert.strictEqual(
+      userTokenAccountInfo.amount.toString(),
+      userOneUnderlyingTokenCurrentBalance.toString()
+    );
+  });
+
+  it("Depositing amount that would make vault exceed the vault deposit limit should revert", async function () {
+    this.qaseId(64);
+    const depositAmount = 5000000000001;
+
+    try {
+      await vaultProgram.methods
+        .deposit(new BN(depositAmount))
+        .accounts({
+          vault: vaultOne,
+          user: userOne.publicKey,
+          userTokenAccount: userOneTokenAccount,
+          userSharesAccount: userOneVaultOneSharesAccount,
+        })
+        .signers([userOne])
+        .rpc();
+    } catch (err) {
+      expect(err.message).contains(
+        "ExceedDepositLimit. Error Number: 6017. Error Message: Exceed deposit limit."
+      );
+    }
+
+    const vaultTokenAccountInfo = await token.getAccount(
+      connection,
+      vaultTokenAccountOne
+    );
+    assert.strictEqual(
+      vaultTokenAccountInfo.amount.toString(),
+      vaultOneCurrentTokenBalance.toString()
+    );
+
+    const userSharesAccountInfo = await token.getAccount(
+      connection,
+      userOneVaultOneSharesAccount
+    );
+    assert.strictEqual(
+      userSharesAccountInfo.amount.toString(),
+      userOneVaultOneSharesCurrentBalance.toString()
+    );
+
+    const userTokenAccountInfo = await token.getAccount(
+      connection,
+      userOneTokenAccount
+    );
+    assert.strictEqual(
+      userTokenAccountInfo.amount.toString(),
+      userOneUnderlyingTokenCurrentBalance.toString()
+    );
+  });
+
+  it("Depositing into the same vault with multiple users is successful", async function () {
+    this.qaseId(65);
+    const depositAmount = 50000000000;
+
+    await vaultProgram.methods
+      .deposit(new BN(depositAmount))
+      .accounts({
+        vault: vaultOne,
+        user: userOne.publicKey,
+        userTokenAccount: userOneTokenAccount,
+        userSharesAccount: userOneVaultOneSharesAccount,
+      })
+      .signers([userOne])
+      .rpc();
+
+    await vaultProgram.methods
+      .deposit(new BN(depositAmount))
+      .accounts({
+        vault: vaultOne,
+        user: userTwo.publicKey,
+        userTokenAccount: userTwoTokenAccount,
+        userSharesAccount: userTwoVaultOneSharesAccount,
+      })
+      .signers([userTwo])
+      .rpc();
+
+    userOneUnderlyingTokenCurrentBalance -= depositAmount;
+    userOneVaultOneSharesCurrentBalance += depositAmount;
+    vaultOneCurrentTokenBalance += depositAmount * 2;
+    userTwoUnderlyingTokenCurrentBalance -= depositAmount;
+    userTwoVaultOneSharesCurrentBalance += depositAmount;
+
+    const vaultTokenAccountOneInfo = await token.getAccount(
+      connection,
+      vaultTokenAccountOne
+    );
+    assert.strictEqual(
+      vaultTokenAccountOneInfo.amount.toString(),
+      vaultOneCurrentTokenBalance.toString()
+    );
+
+    const userSharesAccountOneInfo = await token.getAccount(
+      connection,
+      userOneVaultOneSharesAccount
+    );
+    assert.strictEqual(
+      userSharesAccountOneInfo.amount.toString(),
+      userOneVaultOneSharesCurrentBalance.toString()
+    );
+
+    const userTokenAccountOneInfo = await token.getAccount(
+      connection,
+      userOneTokenAccount
+    );
+    assert.strictEqual(
+      userTokenAccountOneInfo.amount.toString(),
+      userOneUnderlyingTokenCurrentBalance.toString()
+    );
+
+    const userSharesAccountTwoInfo = await token.getAccount(
+      connection,
+      userTwoVaultOneSharesAccount
+    );
+    assert.strictEqual(
+      userSharesAccountTwoInfo.amount.toString(),
+      userTwoVaultOneSharesCurrentBalance.toString()
+    );
+    const userTokenAccountTwoInfo = await token.getAccount(
+      connection,
+      userTwoTokenAccount
+    );
+    assert.strictEqual(
+      userTokenAccountTwoInfo.amount.toString(),
+      userTwoUnderlyingTokenCurrentBalance.toString()
+    );
+  });
+
+  it("Depositing into different vaults with the same user is successful", async function () {
+    this.qaseId(66);
+    const depositAmount = 50000000000;
+
+    await vaultProgram.methods
+      .deposit(new BN(depositAmount))
+      .accounts({
+        vault: vaultOne,
+        user: userOne.publicKey,
+        userTokenAccount: userOneTokenAccount,
+        userSharesAccount: userOneVaultOneSharesAccount,
+      })
+      .signers([userOne])
+      .rpc();
+
+    await vaultProgram.methods
+      .deposit(new BN(depositAmount))
+      .accounts({
+        vault: vaultTwo,
+        user: userOne.publicKey,
+        userTokenAccount: userOneTokenAccount,
+        userSharesAccount: userOneVaultTwoSharesAccount,
+      })
+      .signers([userOne])
+      .rpc();
+
+    userOneUnderlyingTokenCurrentBalance -= depositAmount * 2;
+    userOneVaultOneSharesCurrentBalance += depositAmount;
+    userOneVaultTwoSharesCurrentBalance += depositAmount;
+    vaultOneCurrentTokenBalance += depositAmount;
+    vaultTwoCurrentTokenBalance += depositAmount;
+
+    const vaultTokenAccountOneInfo = await token.getAccount(
+      connection,
+      vaultTokenAccountOne
+    );
+    const vaultTokenAccountTwoInfo = await token.getAccount(
+      connection,
+      vaultTokenAccountTwo
+    );
+    assert.strictEqual(
+      vaultTokenAccountOneInfo.amount.toString(),
+      vaultOneCurrentTokenBalance.toString()
+    );
+    assert.strictEqual(
+      vaultTokenAccountTwoInfo.amount.toString(),
+      vaultTwoCurrentTokenBalance.toString()
+    );
+
+    const userSharesAccountOneInfo = await token.getAccount(
+      connection,
+      userOneVaultOneSharesAccount
+    );
+    assert.strictEqual(
+      userSharesAccountOneInfo.amount.toString(),
+      userOneVaultOneSharesCurrentBalance.toString()
+    );
+    const userSharesAccountTwoInfo = await token.getAccount(
+      connection,
+      userOneVaultTwoSharesAccount
+    );
+    assert.strictEqual(
+      userSharesAccountTwoInfo.amount.toString(),
+      userOneVaultTwoSharesCurrentBalance.toString()
+    );
+
+    const userTokenAccountInfo = await token.getAccount(
+      connection,
+      userOneTokenAccount
+    );
+    assert.strictEqual(
+      userTokenAccountInfo.amount.toString(),
+      userOneUnderlyingTokenCurrentBalance.toString()
+    );
+  });
+
+  it("Depositing into a vault that is shut down should revert", async function () {
+    this.qaseId(67);
+    const depositAmount = 100000000000;
+
+    try {
+      await vaultProgram.methods
+        .deposit(new BN(depositAmount))
+        .accounts({
+          vault: vaultThree,
+          user: userOne.publicKey,
+          userTokenAccount: userOneTokenAccount,
+          userSharesAccount: userOneVaultThreeSharesAccount,
+        })
+        .signers([userOne])
+        .rpc();
+    } catch (err) {
+      expect(err.message).contains(
+        "VaultShutdown. Error Number: 6000. Error Message: Vault was shutdown."
+      );
+    }
+
+    const vaultTokenAccountInfo = await token.getAccount(
+      connection,
+      vaultTokenAccountOne
+    );
+    assert.strictEqual(
+      vaultTokenAccountInfo.amount.toString(),
+      vaultOneCurrentTokenBalance.toString()
+    );
+
+    const userSharesAccountInfo = await token.getAccount(
+      connection,
+      userOneVaultOneSharesAccount
+    );
+    assert.strictEqual(
+      userSharesAccountInfo.amount.toString(),
+      userOneVaultOneSharesCurrentBalance.toString()
     );
 
     const userTokenAccountInfo = await token.getAccount(
