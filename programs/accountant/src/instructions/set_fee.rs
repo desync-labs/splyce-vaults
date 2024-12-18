95fc24ef68656d26c2d64298c0980205b005ef62
Vitalii Koval <vitalii.koval.dev@gmail.com>
2024-12-18 11:40:50 +0400

    add events

--- programs/accountant/src/instructions/set_fee.rs
+++ programs/accountant/src/instructions/set_fee.rs
@@ -5,6 +5,7 @@ use access_control::{
     state::{UserRole, Role}
 };
 
+use crate::events::{EntryFeeUpdatedEvent, PerformanceFeeUpdatedEvent, RedemptionFeeUpdatedEvent};
 use crate::utils::unchecked_accountant::UncheckedAccountant;
 
 #[derive(Accounts)]
@@ -37,7 +38,14 @@ pub fn handle_set_performance_fee(
     let accountant = &mut ctx.accounts.accountant.from_unchecked()?;
 
     accountant.set_performance_fee(fee)?;
-    accountant.save_changes(&mut &mut ctx.accounts.accountant.try_borrow_mut_data()?[8..])
+    accountant.save_changes(&mut &mut ctx.accounts.accountant.try_borrow_mut_data()?[8..])?;
+
+    emit!(PerformanceFeeUpdatedEvent {
+        accountant_key: ctx.accounts.accountant.key(),
+        performance_fee: fee,
+    });
+
+    Ok(())
 }
 
 
@@ -48,7 +56,14 @@ pub fn handle_set_entry_fee(
     let accountant = &mut ctx.accounts.accountant.from_unchecked()?;
 
     accountant.set_entry_fee(fee)?;
-    accountant.save_changes(&mut &mut ctx.accounts.accountant.try_borrow_mut_data()?[8..])
+    accountant.save_changes(&mut &mut ctx.accounts.accountant.try_borrow_mut_data()?[8..])?;
+
+    emit!(EntryFeeUpdatedEvent {
+        accountant_key: ctx.accounts.accountant.key(),
+        entry_fee: fee,
+    });
+
+    Ok(())
 }
 
 pub fn handle_set_redemption_fee(
@@ -58,6 +73,13 @@ pub fn handle_set_redemption_fee(
     let accountant = &mut ctx.accounts.accountant.from_unchecked()?;
 
     accountant.set_redemption_fee(fee)?;
-    accountant.save_changes(&mut &mut ctx.accounts.accountant.try_borrow_mut_data()?[8..])
+    accountant.save_changes(&mut &mut ctx.accounts.accountant.try_borrow_mut_data()?[8..])?;
+
+    emit!(RedemptionFeeUpdatedEvent {
+        accountant_key: ctx.accounts.accountant.key(),
+        redemption_fee: fee,
+    });
+
+    Ok(())
 }
 
--- programs/accountant/src/lib.rs
+++ programs/accountant/src/lib.rs
@@ -2,6 +2,7 @@ use anchor_lang::prelude::*;
 
 pub mod constants;
 pub mod error;
+pub mod events;
 pub mod instructions;
 pub mod state;
 pub mod utils;
